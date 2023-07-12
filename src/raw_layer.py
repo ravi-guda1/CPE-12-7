9a9090ddecfe0c8502d9a4ca6da7b59384c2f8dd
ravi <ravikumar.g@kloud9.nyc>
2023-07-12 11:43:28 +0530

    added

--- src/raw_layer.py
+++ src/raw_layer.py
@@ -1,16 +1,17 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path.cwd().parent))
-
+from pyspark.sql import SparkSession
 from helpers.spark_helper import SparkHelper
 from helpers.snowflake_helper import SnowflakeHelper
 from helpers.local_helper import LocalHelper
 from helpers.hive_helper import HiveHelper
 import env
+import json
 
-def load_csv():
+def load_json():
     spark=SparkHelper.get_spark_session()
-    raw_df = spark.read.csv(r"D:\retail_sales\src\inputs\Retail.csv", header=True).coalesce(1)
+    raw_df = spark.read.option("multiline","true").json(r"E:\CPE-12-7-23\src\inputs\AdventureWorksSales2017-210509-235702.json", header=True).coalesce(1)
     # raw_df = raw_df.na.drop()
     return raw_df
 
@@ -25,7 +26,7 @@ def to_snowflake(df):
     SnowflakeHelper().save_df_to_snowflake(df, env.raw_layer_sf_table)
 
 if __name__=="__main__":
-    df=load_csv()
+    df=load_json()
     to_local(df)
     to_hive(df)
     to_snowflake(df)
